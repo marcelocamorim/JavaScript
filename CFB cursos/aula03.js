7401498a1556e034bec1c50d62333659bb058bf8
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-08-21 02:07:58 -0300

    entendento variaveis

---
+++ CFB cursos/aula03.js
@@ -0,0 +1,13 @@
+"use strict"
+
+function teste(){
+    let nome="marcelo ok"
+    if(true){
+        console.log(" dentro do IF do teste: " + nome)
+    }
+    console.log("dentro do teste: " + nome)
+}
+
+teste()
+    
+console.log("fora do teste: " + nome)
