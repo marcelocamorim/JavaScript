ec023ecd1d66ec8154c7d513a2097eac5b7a104f
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-08-24 00:37:51 -0300

    aula 4 e 5

--- CFB cursos/aula04.js
+++ CFB cursos/aula04.js
@@ -1,8 +1,7 @@
 let num1=0,num2=0,res=0
 
-num1=5
-num2=10
+num1=10
 
-res=num1+num2
+num1*=5
 
-console.log(num2/num1)
+console.log(num1)
---
+++ CFB cursos/aula05.js
@@ -0,0 +1,15 @@
+/*
+Operadores Relacionais
+> maior
+< menor 
+>= maior ou igual
+<= menor ou igual
+== igual
+!= diferente
+*/
+
+let num1=10
+let num2=5
+let num3=10
+
+console.log(!(num1 == num3))
