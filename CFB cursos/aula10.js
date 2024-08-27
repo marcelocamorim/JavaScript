2d754daaff76efbad91b4f3c25c3c0bcc870e2d3
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-08-26 21:18:14 -0300

    Create aula10.js

---
+++ CFB cursos/aula10.js
@@ -0,0 +1,18 @@
+let num=10
+
+res = (num%2 ? "par" : "impar")
+console.log(res)
+
+
+/*
+
+num =10 % 2 (o resto da divisão de 10 por 2)
+? =(operador ternário) similar a if else
+"par" =(1° opção) caso true
+"impar" = (2° opção) caso false
+
+
+
+0 = false
+1 = true
+*/
