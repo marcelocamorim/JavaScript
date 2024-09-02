e6a46d6df423490cd03a6adeb2047f253a3cd837
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-01 22:03:23 -0300

    Create aula13.js

---
+++ CFB cursos/aula13.js
@@ -0,0 +1,24 @@
+//Comando condicional IF e IF ELSE 
+
+// comando if
+
+let num=100
+
+if(num > 10){
+    console.log('Número maior que 10')
+    if(num>50){
+        console.log('Número maior que 50')
+    }
+}else if(num >5){
+    console.log('número está entre 6 e 10')
+}else{
+    console.log('Número igual ou menor que 5')
+}
+
+console.log('Fim do Programa')
+
+//quando usado apenas IF se a condição especificada for 'true' ele executa a tarefa caso seja false ele ignora a tarefa e continua o código
+
+// quando usade 'if' e 'else' quando if for true executa sua função caso if seja false, irá executar a função em "else"
+
+    
