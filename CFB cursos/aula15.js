e3895a079e3b2cbbe1f8be2dd02f643652c2c3e7
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-03 13:07:09 -0300

    Create aula15.js

---
+++ CFB cursos/aula15.js
@@ -0,0 +1,15 @@
+//Loop FOR
+
+/*
+for (inicialização; condição; incremento) {
+         Código a ser executado a cada iteração
+}
+*/
+
+console.log("Inicio do programa")
+
+for(let i=0; i < 10; i++){
+    console.log("CFB Cursos")
+}
+
+console.log("Fim do programa")
