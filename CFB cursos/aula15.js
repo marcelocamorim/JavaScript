0b3e18bd5348e6c2ca64cbf77d00c021deeb0190
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-04 12:13:05 -0300

    Update aula15.js

--- CFB cursos/aula15.js
+++ CFB cursos/aula15.js
@@ -8,8 +8,14 @@ for (inicialização; condição; incremento) {
 
 console.log("Inicio do programa")
 
-for(let i=0; i < 10; i++){
-    console.log("CFB Cursos")
+for(let i=0; i <= 10; i++){
+    if(i%2==0){
+        console.log(i+ " Par ")
+    }else{
+        console.log(i+" ìmpar")
+    }
+        
+
 }
 
 console.log("Fim do programa")
