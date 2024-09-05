41d248d9555354fc03bc198536fc4efdfc65f14e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-05 19:57:53 -0300

    Update aula16.js

--- CFB cursos/aula16.js
+++ CFB cursos/aula16.js
@@ -1,7 +1,27 @@
+// For of e For in
+
+const objs=document.getElementsByTagName("div")
 let num=[10,20,30,40,50]
 
+for(let o in objs){
+    console.log(o)
+}
+//exemplo de for in.
+//itera sobre as posições dos elementos do array, o indice do array num, saída 0, 1, 2, 3, 4
+//igual ao comando abaixo
+/*
+for(let i=0; i<num.length;i++){
+    console.log(i)
+}
+*/
+
+for(let o of objs){
+    console.log(o)
+}
+/*
+Exemplo de for of.
+itera diretamente sobre os elementos dentro do array num, saída: 10, 20, 30, 40, 50
+igual exemplo abaixo
 for(let i=0; i<num.length;i++){
     console.log(num[i])
-}
-
-
+*/
