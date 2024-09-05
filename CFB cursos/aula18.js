39fad2dd2d1fc33798ed8b276e5960d26da74155
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-05 20:41:46 -0300

    Create aula18.js

---
+++ CFB cursos/aula18.js
@@ -0,0 +1,12 @@
+//Loop Do While
+
+let n=10
+
+do{ 
+    console.log("testando Do while") 
+    n++
+}while(n<10)
+
+console.log("Fim do programa")
+
+//o loop do while primeiro executa a função e depois verifica a condição
