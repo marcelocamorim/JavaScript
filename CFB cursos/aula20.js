007cecdb0ab9895ddc680d0d31f2ce3bd1cc8b1f
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-06 00:10:50 -0300

    Create aula20.js

---
+++ CFB cursos/aula20.js
@@ -0,0 +1,7 @@
+// Funções em JavaScript
+
+function nome(){ //function definida como "nome"
+    console.log("CFB Cursos") // ação a ser executada pela function
+}
+
+nome() //chamada para execução da function
