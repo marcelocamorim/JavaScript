ee7de8447ca8792e7a4fac5b7542b9e9eb50503d
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-08 12:04:57 -0300

    Update aula22.js

--- CFB cursos/aula22.js
+++ CFB cursos/aula22.js
@@ -1,5 +1,21 @@
 function soma (n1,n2){
-    console.log(n1+n2)
+    return n1+n2
+}
+let resultado=soma(10,10)
+
+//console.log(resultado)
+
+
+let valor=0
+
+function add(v){
+    valor+=v
 }
 
-soma(10,10)
+console.log(valor)
+
+add(10)
+console.log(valor)
+
+add(25)
+console.log(valor)
