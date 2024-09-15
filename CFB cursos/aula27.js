306063cc740fcef6ac746ac6b0770e07e18e63af
marcelo c. amorim <marceloamorim.dev@gmail.com>
2024-09-15 11:49:27 -0300

    Create aula27.js

---
+++ CFB cursos/aula27.js
@@ -0,0 +1,18 @@
+// Funções geradoras
+//tipo especial de função que podem ser pausadas e retomadas. Elas retornam um objeto que implementa o protocolo de iterador, permitindo gerar valores conforme necessário, ao invés de calculá-los todos de uma vez. 
+
+
+function* cores(){ //function com * significa que é uma função geradora
+    yield 'vermelho' 
+    yield 'verde'
+    yield 'azul'
+}
+//Cada vez que yield é encontrado, a execução da função é pausada e o valor especificado após yield é retornado.
+
+let itc=cores() //variavel recebe chamada da function
+console.log(itc.next().value) 
+console.log(itc.next().value)
+console.log(itc.next().value)
+
+//.next() é a proxima execução da function
+//.value é o valor após yield
