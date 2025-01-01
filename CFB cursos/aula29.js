ff7a9476b16f08114e110053d5bd0a492b7be78e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-01 13:12:54 -0300

    Update aula29.js
    revisão if

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,19 +1,14 @@
 
-let num=50
-
+let num=52
 if(num>=10){
-    console.log("número maior ou igual a 10")
-    if(num>50){
-        console.log('número maior que 50')
-    }else{
-        console.log("número está entre 10 e 50")
+    console.log('Número maior 10')
+    if(num >50){
+        console.log('Número maior que 50')
+        if(num>50 && num<55){
+            console.log('Número está entre 50 e 55')
+        }
     }
-}else if(num>5){
-    console.log("número está entre 6 e 10")
-}else{
-    console.log("número menor ou igual a 5")
 }
-
 
 
 console.log("fim do programa")
