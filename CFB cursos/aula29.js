b8ed4ce060fe49fdb1e3d1b6b66a6f12628fadf4
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-01 13:29:20 -0300

    Update aula29.js
    revisao switch case

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,14 +1,19 @@
+let colocacao=5
 
-let num=52
-if(num>=10){
-    console.log('Número maior 10')
-    if(num >50){
-        console.log('Número maior que 50')
-        if(num>50 && num<55){
-            console.log('Número está entre 50 e 55')
-        }
-    }
+switch(colocacao){
+    case 1:
+        console.log('primeiro lugar')
+        break
+    case 2:
+        console.log('segundo lugar')
+        break
+    case 3:
+        console.log('terceiro lugar')
+        break
+    default:
+        console.log('não participou')
+        break
 }
 
 
-console.log("fim do programa")
+
