9b5c2f49fbc5ab06f7bd3e3f5b9bc271bf02a980
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-02 13:24:31 -0300

    Update aula29.js
    revisão lopps, break e continue

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,8 +1,12 @@
-let n=10
+let n=0
+let max =10
 
-do{
-    console.log('cfb cursos')
+while(n<max){
     n++
-}while(n<10)
+    if(n==5 || n==4 || n==6){
+        continue
+    }
+    console.log('CFB curso '+n )
+}
 
 console.log('fim do programa')
