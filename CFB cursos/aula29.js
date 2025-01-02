26264f4409cd592ae5af2ed04b2b8d4d84a1d100
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-01 21:23:50 -0300

    Update aula29.js
    revisão de loops

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,8 +1,8 @@
-for(let i=0; i<10; i++){
-    if(i%2==0){
-        console.log(i + ' é par')
-    }else{
-        console.log(i + ' é impar')
-    }
-}
+let n=10
+
+do{
+    console.log('cfb cursos')
+    n++
+}while(n<10)
+
 console.log('fim do programa')
