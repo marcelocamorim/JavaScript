ba562366b7529cde5395cf77afbbb9d51502d6f1
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-09 12:58:15 -0300

    Update aula29.js
    revisão

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,10 +1,6 @@
-function* perguntas(){
-    const nome=yield 'qual seu nome'
-    const esporte=yield 'qual seu esporte favorito'
-    return 'seu nome é ' + nome + ' seu esporte favorito é ' + esporte
-}
+let n=0
 
-const itp=perguntas()
-console.log(itp.next().value)
-console.log(itp.next('marcelo').value)
-console.log(itp.next('Muay Thai').value)
+while(n<10){
+    console.log('testando while '+n)
+    n++
+}
