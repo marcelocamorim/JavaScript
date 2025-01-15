02884ba2a141611d59eac81f601bf1aec18de581
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-14 22:58:16 -0300

    revisão
    revisão

--- CFB cursos/aula29.js
+++ CFB cursos/aula29.js
@@ -1,9 +1,4 @@
-let n=0
-
-do{
-    n++
-    if((n==5), (n==6)){
-        continue
-    }
-    console.log('testando do while' + n)
-}while(n<10)
+const cursos=['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+cursos.map((el,i)=>{
+    console.log('curso:'+el+'- posição do curso:'+i)
+})
