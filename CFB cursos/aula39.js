633ea6c11fd31f725323f68d09b125f01e9286e3
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-27 12:29:50 -0300

    create aula 39

---
+++ CFB cursos/aula39.js
@@ -0,0 +1,12 @@
+//parents and childrens 
+
+const caixa1=document.querySelector("#caixa1")
+const curso=[...document.querySelectorAll(".curso")]
+const c1_2=document.querySelector("#c1_2")
+
+
+
+console.log(c1_2.parentNode.parentNode.children[2])//pega o filho2 do pai do pai do elemento
+
+//console.log(caixa1.children[1].innerHTML="teste teste") //pegando children
+
