203bc40209c0df33ee79feee24477ffc7c686ce1
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-01-29 21:52:56 -0300

    create aula 41

--- CFB cursos/aula41.js
+++ CFB cursos/aula41.js
@@ -6,6 +6,15 @@ cursos.map((el,i)=>{
     novoElemento.setAttribute("id","c"+i)
     novoElemento.setAttribute("class", "curso c1")
     novoElemento.innerHTML=(el)
+
+    const btn_lixeira=document.createElement("img")
+    btn_lixeira.setAttribute("src","./aula41lixeira.png")
+    btn_lixeira.setAttribute("class", "btn_lixeira")
+    btn_lixeira.addEventListener("click", (evt)=>{
+        caixa1.removeChild(evt.target.parentNode)
+    })
+   
+    novoElemento.appendChild(btn_lixeira)
     caixa1.appendChild(novoElemento)
 })
 
