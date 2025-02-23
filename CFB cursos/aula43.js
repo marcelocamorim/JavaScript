30b605dc7e3c7d5808efa6fd37cc99d9d06180f6
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-02-23 19:26:00 -0300

    create aula 43

---
+++ CFB cursos/aula43.js
@@ -0,0 +1,36 @@
+const caixaCursos = document.querySelector("#caixaCursos")
+const btn_c = [...document.querySelectorAll(".curso")]
+const c1_2 = document.querySelector("#c1_2")
+const cursos = ["HTML", "CSS", "JavaScript", "PHP", "React", "MySQL", "ReactNative"]
+const btnCursoSelecionado=document.getElementById("btnCursoSelecionado")
+
+cursos.map((el,i)=>{
+    const novoElemento=document.createElement("div")
+    novoElemento.setAttribute("id", "c"+i)
+    novoElemento.setAttribute("class", "curso c1")
+    novoElemento.innerHTML=el
+
+    const comandos=document.createElement("div")
+    comandos.setAttribute("class", "comandos")
+
+    const rb=document.createElement("input")
+    rb.setAttribute("type", "radio")
+    rb.setAttribute("name", "rb_curso")
+
+    comandos.appendChild(rb)
+
+    novoElemento.appendChild(comandos)
+
+    caixaCursos.appendChild(novoElemento)
+})
+
+btnCursoSelecionado.addEventListener("click", (evt)=>{
+    const todosRadios=[...document.querySelectorAll("input[type=radio]")]
+    const radioSelecionado=todosRadios.filter((el)=>{
+        return el.checked
+    })
+    console.log(todosRadios)
+    console.log(radioSelecionado)
+})
+
+
