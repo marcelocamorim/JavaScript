95b8df7d8e63a794ba7186f04bcdd254041f4caf
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-03-12 20:32:53 -0300

    Create aula53_desafio copy.js

---
+++ CFB cursos/aula53_desafio copy.js
@@ -0,0 +1,36 @@
+const btn_soma=document.querySelector("#btn_Soma")
+const btn_subtracao=document.querySelector("#btn_subtracao")
+const btn_Multiplicacao=document.querySelector("#btn_Multiplicacao")
+const btn_Divisao=document.querySelector("#btn_Divisao")
+
+const valor1=document.getElementById("valor1")
+const valor2=document.getElementById("valor2")
+const res=document.querySelector("#res")
+
+
+const op=[
+    ()=>{
+        const val=[Number(valor1.value),Number(valor2.value)]
+        res.value=val[0]+val[1]
+    },
+    ()=>{
+        const val=[Number(valor1.value),Number(valor2.value)]
+        res.value=val[0]-val[1]
+    },
+    ()=>{
+        const val=[Number(valor1.value),Number(valor2.value)]
+        res.value=val[0]*val[1]
+    },
+    ()=>{
+        const val=[Number(valor1.value),Number(valor2.value)]
+        res.value=val[0]/val[1]
+    }
+]
+
+
+btn_soma.addEventListener("click",op[0])
+btn_subtracao.addEventListener("click",op[1])
+btn_Multiplicacao.addEventListener("click",op[2])
+btn_Divisao.addEventListener("click",op[3])
+
+
