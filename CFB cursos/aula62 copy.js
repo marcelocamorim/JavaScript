6f4b55152719a8996abbe60769c0279a89d16885
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-04-02 20:54:52 -0300

    revisão da aula62 e 63

--- CFB cursos/aula62 copy.js
+++ CFB cursos/aula62 copy.js
@@ -8,21 +8,25 @@
 // criar metodo para atirar
 //criar dois carros
 
+//adicionar carros no html
+//carro normal bloqueia blindagem e municao
+
+
 
 class Carro{
     constructor(nome,portas){
         this.nome=nome
         this.portas=portas
-        this.ligado=false
+        this.ligar=false
         this.vel=0
         this.cor=undefined
     }
 
-    ligar=function(){
-        this.ligado=true
+    ligado=function(){
+        this.ligar=true
     }
-    desligar=function(){
-        this.ligado=false
+    desligado=function(){
+        this.ligar=false
     }
     setCor=function(cor){
         this.cor=cor
@@ -30,25 +34,16 @@ class Carro{
 }
 
 class Militar extends Carro{
-    constructor(nome, portas, blindagem, municao){
-        super(nome, portas)
+    constructor(nome,portas,blindagem,municao){
         this.blindagem=blindagem
         this.municao=municao
         this.setCor("verde")
     }
 
     atirar=function(){
-        if(this.municao>0)
-            this.municao--
+        if(municao>0){
+            municao--
+        }
     }
 }
 
-const c1=new Carro("fox",4)
-c1.ligar()
-c1.setCor("preto")
-
-const c2=new Militar("tanque",1,100,50)
-c2.atirar()
-c2.atirar()
-c2.atirar()
-c2.atirar()
--- CFB cursos/aula63.js
+++ CFB cursos/aula63.js
@@ -1,5 +1,5 @@
-const f_tipoNormal=document.querySelector("#f_tipoNormal")
 const f_tipoMilitar=document.querySelector("#f_tipoMilitar")
+const f_tipoNormal=document.querySelector("#f_tipoNormal")
 const f_blindagem=document.querySelector("#f_blindagem")
 const f_municao=document.querySelector("#f_municao")
 const f_nome=document.querySelector("#f_nome")
@@ -9,17 +9,21 @@ const btn_addCarro=document.querySelector("#btn_addCarro")
 
 let a_carros=[]
 
-f_tipoMilitar.addEventListener("click",(evt)=>{   
+f_tipoMilitar.addEventListener("click",(evt)=>{
+    f_nome.value=""
+    f_portas.value=0
+    f_blindagem.value=0
+    f_municao.value=0
     f_blindagem.removeAttribute("disabled")
     f_municao.removeAttribute("disabled")
 })
-
-f_tipoNormal.addEventListener("click",(evt)=>{   
+f_tipoNormal.addEventListener("click",(evt)=>{
+    f_nome.value=""
+    f_portas.value=0
     f_blindagem.value=0
     f_municao.value=0
     f_blindagem.setAttribute("disabled","disabled")
     f_municao.setAttribute("disabled","disabled")
-
 })
 
 const gerenciarExibicaoCarros=()=>{
@@ -27,9 +31,12 @@ const gerenciarExibicaoCarros=()=>{
     a_carros.forEach((c)=>{
         const div=document.createElement("div")
         div.setAttribute("class","carro")
-        div.innerHTML=c.nome
+        div.innerHTML=`Nome: ${c.nome}<br/>`
+        div.innerHTML+=`Portas: ${c.portas}<br/>`
+        div.innerHTML+=`Cor: ${c.cor}<br/>`
+        div.innerHTML+=`Blindagem: ${c.blindagem}<br/>`
+        div.innerHTML+=`Munição: ${c.municao}<br/>`
         carros.appendChild(div)
-
     })
 }
 
@@ -37,18 +44,14 @@ btn_addCarro.addEventListener("click",()=>{
     if(f_tipoNormal.checked){
         const c=new Carro(f_nome.value,f_portas.value)
         a_carros.push(c)
+    }else{
+        const c=new Militar(f_nome.value,f_portas.value,f_blindagem.value,f_municao.value)
+        a_carros.push(c)
     }
     gerenciarExibicaoCarros()
 })
 
-
-
-
-
-
-//Entendendo sobre Herança em POO com Javascript - Objetos #P6 - Aula 62
-
-class Carro{ //class Pai / base / super class
+class Carro{ //Classe PAI / BASE
     constructor(nome,portas){
         this.nome=nome
         this.portas=portas
@@ -65,43 +68,46 @@ class Carro{ //class Pai / base / super class
     setCor=function(cor){
         this.cor=cor
     }
-
 }
 
-class Militar extends Carro{ // class filho
+class Militar extends Carro{ //Classe Filho
     constructor(nome,portas,blindagem,municao){
         super(nome,portas)
         this.blindagem=blindagem
-        this.municao=municao  
-        this.setCor("Verde")      // define a cor por padrão, todos serão verdes
+        this.municao=municao
+        this.setCor("Verde")
     }
-
     atirar=function(){
         if(this.municao>0){
             this.municao--
         }
-
     }
 }
 
-const c1=new Carro("normal",4)
+const c1=new Carro("Normal",4)
 c1.ligar()
 c1.setCor("Preto")
 
-const c2=new Militar("Lutardor",1,100,50)
-c2.setCor("Azul")
-
+const c2=new Militar("Lutador",1,100,50)
+//c2.setCor("Azul")
+c2.atirar()
+c2.atirar()
+c2.atirar()
+c2.atirar()
+c2.atirar()
 
 console.log(`Nome: ${c1.nome}`)
 console.log(`Portas: ${c1.portas}`)
 console.log(`Ligado: ${(c1.ligado?"Sim":"Não")}`)
 console.log(`Velocidade: ${c1.vel}`)
 console.log(`Cor: ${c1.cor}`)
-console.log(`----------------`)
+console.log("-------------------------")
 
 console.log(`Nome: ${c2.nome}`)
 console.log(`Portas: ${c2.portas}`)
 console.log(`Ligado: ${(c2.ligado?"Sim":"Não")}`)
 console.log(`Velocidade: ${c2.vel}`)
+console.log(`Blindagem: ${c2.blindagem}`)
+console.log(`Munição: ${c2.municao}`)
 console.log(`Cor: ${c2.cor}`)
-console.log(`----------------`)
+console.log("-------------------------")
