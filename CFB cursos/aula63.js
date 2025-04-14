ef8fb15138f0c9ff23a0a026e82a44aea03ab171
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-04-14 11:27:34 -0300

    revisão aula 63

--- CFB cursos/aula63.js
+++ CFB cursos/aula63.js
@@ -1,77 +1,109 @@
 //pegar valores dos campos para criar os carros
-//pegars os tipos radios
+//pegar os tipos radios
 //pegar botao
 // pegar div onde vai os carros
+//bloquear e desbloquear blindagem e municao de acordo com o tipo selecionado
+//limpar os campos sempre que trocar o tipo de carro
 //criar carros com botão dependendo do tipo selecionado
 // add os carros em um array
 //iterar array para criar objs html- nome, portas, blindagem, municao
 //adicionar carros na div a baixo
-//normal bloqueia blindagem e municao
-//ao trocar o tipo limpa os campos
-//
 
+
+//campos
 const f_nome=document.querySelector("#f_nome")
 const f_portas=document.querySelector("#f_portas")
 const f_blindagem=document.querySelector("#f_blindagem")
 const f_municao=document.querySelector("#f_municao")
 
+//radios
 const f_tipoMilitar=document.querySelector("#f_tipoMilitar")
 const f_tipoNormal=document.querySelector("#f_tipoNormal")
 
+//button
 const btn_addCarro=document.querySelector("#btn_addCarro")
+
+//div
 const carros=document.querySelector("#carros")
 
+//bloqueia, desbloqueia e limpa os campos
+f_tipoMilitar.addEventListener("click",(evt)=>{
+    f_nome.value=""
+    f_portas.value=0
+    f_blindagem.value=0
+    f_municao.value=0
+    f_blindagem.disabled=false
+    f_municao.disabled=false
+})
 
 f_tipoNormal.addEventListener("click",(evt)=>{
     f_nome.value=""
     f_portas.value=0
     f_blindagem.value=0
     f_municao.value=0
-    f_blindagem.disabled = true
-    f_municao.disabled = true
+    f_blindagem.disabled=true
+    f_municao.disabled=true
 })
 
-f_tipoMilitar.addEventListener("click",(evt)=>{
-    f_nome.value=""
-    f_portas.value=0
-    f_blindagem.value=0
-    f_municao.value=0
-    f_blindagem.disabled = false
-    f_municao.disabled = false
-})
-
-
-
+//array dos carros
 const a_carros=[]
 
 const gerenciarExibicaoCarros=()=>{
     carros.innerHTML=""
     a_carros.map((c)=>{
         const div=document.createElement("div")
-        div.setAttribute("class", "carro")        
+        div.setAttribute("class", "carro")
         div.innerHTML=`Nome: ${c.nome}<br>`
         div.innerHTML+=`Portas: ${c.portas}<br>`
         div.innerHTML+=`Cor: ${c.cor}<br>`
 
         if(c instanceof Militar){
-            div.innerHTML+=`Blindagem: ${c.blindagem}<br>`
-            div.innerHTML+=`Municao: ${c.municao}<br>`
+            div.innerHTML+=`Blindagem: ${c.blindagem}`
+            div.innerHTML+=`Munição: ${c.municao}`
         }
-        
+
         carros.appendChild(div)
     })
 }
 
-btn_addCarro.addEventListener("click",(evt)=>{
+const limparCampos=()=>{
+    f_nome.value=""
+    f_portas.value=0
+    f_blindagem.value=0
+    f_municao.value=0  
+}
+
+//cria os carros com o button
+btn_addCarro.addEventListener("click",()=>{  
+
+    if (f_nome.value === "") {
+        alert("Por favor, digite o nome do carro.")
+        limparCampos()
+        return
+    }
+
+
+    if (f_portas.value < 0 || f_blindagem.value < 0 || f_municao.value < 0) {
+        alert("Os valores não podem ser negativos!")
+        limparCampos()
+        return      
+    }
+
+
     if(f_tipoNormal.checked){
-        const c=new Carro (f_nome.value, f_portas.value)
+        const c=new Carro(f_nome.value, f_portas.value)
         a_carros.push(c)
     }else{
         const c=new Militar(f_nome.value, f_portas.value, f_blindagem.value, f_municao.value)
         a_carros.push(c)
-    }
+    } 
     gerenciarExibicaoCarros()
+
 })
+
+
+
+
 
 
 
@@ -82,8 +114,7 @@ class Carro{ //Classe PAI / BASE
         this.portas=portas
         this.ligado=false
         this.vel=0
-        this.cor=undefined
-        this.setCor("Preto")
+        this.cor="preto"
     }
     ligar=function(){
         this.ligado=true
