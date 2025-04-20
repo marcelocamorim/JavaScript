a1e591e2483d2cc116daed6216a0709c19f7dec0
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-04-20 14:52:27 -0300

    Create aula65.js

---
+++ CFB cursos/aula65.js
@@ -0,0 +1,21 @@
+//Aprendendo sobre conversões de JSON e Objetos em Javascript - Aula 65
+
+const pessoa={
+    nome:"Bruno",
+    canal:"CFB Cursos",
+    curso:"JavaScript",
+    aulas: {
+        aula01: "Introdução",
+        aula02: "Variáveis",
+        aula03: "Condicional",     
+    },
+}
+
+const string_pessoa='{"nome":"Bruno","canal":"CFB Cursos","curso":"JavaScript","aulas":{"aula01":"Introdução","aula02":"Variáveis","aula03":"Condicional"}}'
+
+const s_json_pessoa=JSON.stringify(pessoa) // converte objeto em string fson
+const o_json_pessoa=JSON.parse(s_json_pessoa)//converte jsonem objeto
+
+
+console.log(pessoa)
+console.log(s_json_pessoa)
