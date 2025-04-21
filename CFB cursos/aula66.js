6263fd84b8ff9f91f465d8a13ecb15bc7e03f0d6
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-04-20 21:45:12 -0300

    create aula66

---
+++ CFB cursos/aula66.js
@@ -0,0 +1,26 @@
+class Npc{
+    static alerta=false
+    constructor(energia){
+        this.energia=energia
+        
+    }
+    info=function(){
+        console.log(`Energia: ${this.energia}`)
+        console.log(`Alerta: ${(Npc.alerta?"sim":"Não")}`)
+        console.log("--------------------")
+    }
+
+    static alertar=function(){
+        Npc.alerta=true
+    }
+}
+
+const npc1=new Npc(100)
+const npc2=new Npc(80)
+const npc3=new Npc(30)
+
+//Npc.alertar()
+
+npc1.info()
+npc2.info()
+npc3.info()
