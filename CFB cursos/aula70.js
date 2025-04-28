ed249a93e93a3f253adbf28aad768fd50ccf1f77
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-04-28 18:36:16 -0300

    att aula 70

---
+++ CFB cursos/aula70.js
@@ -0,0 +1,21 @@
+const teclasNum=[...document.querySelectorAll(".num")]
+const teclasOp=[...document.querySelectorAll(".op")]
+const teclaRes=document.querySelector(".res")
+const display=document.querySelector(".display")
+const teclaOn=document.querySelector("#ton")
+const limpar=document.querySelector("#tlimpar")
+
+teclasNum.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML+=evt.target.innerHTML
+    })
+})
+teclasOp.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        display.innerHTML+=evt.target.innerHTML
+    })
+})
+
+limpar.addEventListener("click",()=>{
+    display.innerHTML="0"
+})
