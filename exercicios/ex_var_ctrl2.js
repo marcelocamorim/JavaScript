7482edbb8f286ae01a90f7d889e1cc0a41cb5fed
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-05-18 12:31:02 -0300

    Create ex_var_ctrl2.js

---
+++ exercicios/ex_var_ctrl2.js
@@ -0,0 +1,40 @@
+/*
+ Exercício 2 – Impedir duas vírgulas no mesmo número
+Objetivo: Criar um campo de entrada numérica com vírgula.
+
+Desafio:
+
+Crie botões de 0 a 9 e vírgula ,
+
+Só deve permitir 1 vírgula por número
+
+Quando o usuário digitar um operador, resete a variável decimal
+*/
+const teclaNum=[...document.querySelectorAll(".teclaNum")]
+const teclaOp=[...document.querySelectorAll(".teclaOp")]
+
+const display=document.getElementById('display')
+
+let sinal=false
+let decimal=false
+
+teclaNum.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if(display.innerHTML=="0"){
+            display.innerHTML=""
+        }
+        if(display.innerHTML==","){
+            display.innerHTML="0,"
+        }
+        display.innerHTML+=evt.target.innerHTML
+        
+    })
+})
+
+
+teclaOp.map((el)=>{
+    el.addEventListener("click",(evt)=>{
+        if()
+
+    })
+})
