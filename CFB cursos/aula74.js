20007487d30cd0f57c654fae046fc82a2988969a
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-05-18 10:36:35 -0300

    Update aula74.js

--- CFB cursos/aula74.js
+++ CFB cursos/aula74.js
@@ -2,19 +2,21 @@ const teclasNum=[...document.querySelectorAll(".num")]
 const teclasOp=[...document.querySelectorAll(".op")]
 const teclaRes=document.querySelector(".res")
 const display=document.querySelector(".display")
-const tcpy=document.querySelector("#tcpy")
-const limpar=document.querySelector("#tlimpar")
-const teste=document.getElementById('teste')
-const calc_aba=document.getElementById('calc_aba')
-const calc=document.getElementById('calc')
-
+const ton=document.getElementById("ton")
+const tlimpar=document.getElementById("tlimpar")
+const tigual=document.getElementById("tigual")
+const tcpy=document.getElementById("tcpy")
+const teste=document.getElementById("teste")
+const calc_aba=document.getElementById("calc_aba")
+const calc=document.getElementById("calc")
+const img_aba_calc=document.getElementById("img_aba_calc")
 
 let sinal=false
 let decimal=false
 
-teclasNum.map((el)=>{//numeros
+teclasNum.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
-        sinal=false      
+        sinal=false
         if(evt.target.innerHTML==","){
             if(!decimal){
                 decimal=true
@@ -23,19 +25,16 @@ teclasNum.map((el)=>{//numeros
                 }else{
                     display.innerHTML+=evt.target.innerHTML
                 }
-                
             }
         }else{
             if(display.innerHTML=="0"){
                 display.innerHTML=""
-            }
-
+            }            
             display.innerHTML+=evt.target.innerHTML
         }
     })
 })
-
-teclasOp.map((el)=>{//operadores
+teclasOp.forEach((el)=>{
     el.addEventListener("click",(evt)=>{
         if(!sinal){
             sinal=true
@@ -47,18 +46,17 @@ teclasOp.map((el)=>{//operadores
             }else{
                 display.innerHTML+=evt.target.innerHTML
             }
-        
         }
     })
 })
 
-limpar.addEventListener("click",(evt)=>{
+tlimpar.addEventListener("click",(evt)=>{
     sinal=false
     decimal=false
     display.innerHTML="0"
 })
 
-teclaRes.addEventListener("click",(evt)=>{
+tigual.addEventListener("click",(evt)=>{
     sinal=false
     decimal=false
     const res=eval(display.innerHTML)
@@ -66,22 +64,18 @@ teclaRes.addEventListener("click",(evt)=>{
 })
 
 tcpy.addEventListener("click",(evt)=>{
-   // navigator.clipboard.writeText(display.innerHTML)
-   teste.select()
-   teste.setSelectionRange(0,99999)
-   navigator.clipboard.writeText(teste.value)
+    navigator.clipboard.writeText(display.innerHTML)
+    // teste.select()
+    // teste.setSelectionRange(0,99999)//Mobile
+    // navigator.clipboard.writeText(teste.value)
 })
 
 calc_aba.addEventListener("click",(evt)=>{
     calc.classList.toggle("calc_exibir")
+    console.log(evt.target)
+    if(calc.classList.contains("calc_exibir")){
+        evt.target.setAttribute("src","seta_esquerda.svg")
+    }else{
+        evt.target.setAttribute("src","seta_direita.svg")
+    }
 })
-
-
-/*
-if(calc.classList.contains("calc_exibir")){
-    img_aba_calc.setAtribute("src", "seta_esquerda.svg")
-}else{
-    img_aba_calc.setAtribute("src", "seta_direita.svg")
-}
-
-*/
