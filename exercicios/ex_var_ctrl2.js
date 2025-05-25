fa3f1d553a0d31795a2e736f874fc692b9f55f34
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-05-24 23:12:37 -0300

    praticando exercicios

--- exercicios/ex_var_ctrl2.js
+++ exercicios/ex_var_ctrl2.js
@@ -3,13 +3,11 @@
 Objetivo: Criar um campo de entrada numérica com vírgula.
 
 Desafio:
-
 Crie botões de 0 a 9 e vírgula ,
-
 Só deve permitir 1 vírgula por número
-
 Quando o usuário digitar um operador, resete a variável decimal
 */
+
 const teclaNum=[...document.querySelectorAll(".teclaNum")]
 const teclaOp=[...document.querySelectorAll(".teclaOp")]
 
@@ -18,25 +16,51 @@ const display=document.getElementById('display')
 let sinal=false
 let decimal=false
 
+//se click numero mostra visor
+//se click virgula mostar 0,
+//se visor== 0 recebe numero clicado
+//se ultimo digito for op recebe 0,
+//se clicar em numero libera ops
+
 teclaNum.map((el)=>{
     el.addEventListener("click",(evt)=>{
+        const valor=evt.target.innerHTML
         sinal=false
-        if(display.innerHTML=="0"){
-            display.innerHTML=""
-        }         
+        if(valor==","){
+            if(!decimal){
+                decimal=true
+                if(display.innerHTML.endsWith("+") || display.innerHTML.endsWith("-") || display.innerHTML.endsWith("*") || display.innerHTML.endsWith("/")){
+                    display.innerHTML+="0,"
+                }else if(display.innerHTML=="0"){
+                    display.innerHTML="0,"
+                }else{
+                    display.innerHTML+=","
+                }
+            }
+        }else{
+            if(display.innerHTML=="0"){
+                display.innerHTML=valor
+            }else{
+                display.innerHTML+=valor
+            }
+        }
+
         
-        display.innerHTML+=evt.target.innerHTML
-    
+      
     })
 })
 
 
-
 teclaOp.map((el)=>{
     el.addEventListener("click",(evt)=>{
-        if(!decimal){
-            
+        decimal=false
+        if(!sinal){
+            sinal=true
+            const op=evt.target.innerHTML
+            if(op=="x") op="*"                    
+            display.innerHTML+=op
         }
+
 
     })
 })
