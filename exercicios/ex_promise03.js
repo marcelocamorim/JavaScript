4de9fc6231e1ffebb4d7d010092bad01ca70faf4
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-07 18:52:45 -0300

    Update ex_promise03.js

--- exercicios/ex_promise03.js
+++ exercicios/ex_promise03.js
@@ -2,3 +2,24 @@
 Exercício 3 – Simular busca de dados
 Objetivo: Criar uma função que simula buscar dados de um servidor, usando Promise com um setTimeout.
 */
+dadosEncontrados=true
+
+function buscandoDados(){
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            if(dadosEncontrados){
+                res("Arquivos encontrados")
+            }else{
+                rej("Erro! Nenhum arquivo encontrado")
+            }
+            
+        }, 3000);
+    })
+}
+
+buscandoDados()
+    .then((msg)=>{console.log('Sucesso!', msg)})
+    .catch((erro)=>{console.log(erro)})
+
+
+console.log("Buscando informações...")
