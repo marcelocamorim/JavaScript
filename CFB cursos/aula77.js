ac213115cbdefc484cb7b195ccdeb841c15312d0
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-10 12:45:23 -0300

    Update aula77.js

--- CFB cursos/aula77.js
+++ CFB cursos/aula77.js
@@ -1,4 +1,5 @@
 const div_data=document.getElementById("div_data")
+const div_relogio=document.getElementById("div_relogio")
 const data=new Date()
 
 let dia=data.getDate()
@@ -11,7 +12,22 @@ const data_r=dia+"/"+mes+"/"+data.getFullYear()
 
 div_data.innerHTML=data_r
 
+const relogio=()=>{
+    const data=new Date()
 
+    let hora=data.getHours()
+    hora=hora<10?"0"+hora:hora
 
+    let minuto=data.getMinutes()
+    minuto=minuto<10?"0"+minuto:minuto
 
-console.log(data)
+    let segundo=data.getSeconds()
+    segundo=segundo<10?"0"+segundo:segundo
+
+    const hora_completa=`${hora} : ${minuto} : ${segundo}`
+
+    div_relogio.innerHTML=hora_completa
+}
+
+const intervalo=setInterval(relogio,1000)
+
