49d8be32939642148e69b5eb19e5ffd9c8593896
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-13 12:29:50 -0300

    Update ex_datas01.js

--- exercicios/ex_datas01.js
+++ exercicios/ex_datas01.js
@@ -13,22 +13,36 @@ Quando o tempo acabar, mostre a mensagem "Tempo finalizado!" dentro de uma div.
 
 
 const tempo=document.getElementById('tempo')
-const msg=document.getElementById('msg')
-const cont=document.getElementById('cont')
-const tempo_do_alarme=document.getElementById('tempo_do_alarme')
+const msg=document.getElementById('msg')//div menssagem
+const cont=document.getElementById('cont')//botao
+const tempo_do_alarme=document.getElementById('tempo_do_alarme')//input
+
+let intervalo=null
+
 
 cont.addEventListener("click",()=>{
     let tempo=Number(tempo_do_alarme.value)
-    
+    msg.innerHTML="Iniciando..."
+
+    if(isNaN(tempo)||tempo<0){
+        msg.innerHTML="Digite um tempo Válido"
+        return
+    }
+
+    if(intervalo !==null){
+        clearInterval(intervalo)
+        intervalo=null
+    }
+
     intervalo=setInterval(()=>{
         msg.innerHTML=`${tempo} Segundos`
         tempo--
-        if(tempo<=0){
+
+        if(tempo<0){
             clearInterval(intervalo)
-            msg.innerHTML="Tempo Finalizado!"
+            intervalo=null
+            msg.innerHTML="Contagem Finalizada"
         }
-        
     },1000)
 })
-
 
