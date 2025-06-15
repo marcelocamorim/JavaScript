a2eb65feeabbc83831c726f17d6c1bda5149c7e9
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-15 12:28:27 -0300

    praticando exercicios

--- exercicios/ex_datas02.js
+++ exercicios/ex_datas02.js
@@ -16,26 +16,54 @@ Dica: você pode usar uma let contador = 0 e ir incrementando ela dentro do setI
  */
 
 
-const crono=document.getElementById('crono')
-const iniciar=document.getElementById('iniciar')
-const parar=document.getElementById('parar')
+const crono = document.getElementById('crono')
+const iniciar = document.getElementById('iniciar')
+const parar = document.getElementById('parar')
+const pausar=document.getElementById('pausar')
 
-contando=false
-intervalo=null
+let segundos=0
+let minutos=0
+let horas=0
+let intervalo=null
+let contando=false
 
-iniciar.addEventListener("click", ()=>{
-    let tempo=0
-    intervalo=setInterval(()=>{
-        if(!contando){                    
-            crono.innerHTML=tempo      
-            tempo++ 
-        }
+iniciar.addEventListener("click",()=>{
+    if(contando)return//barra outra contagem enquanto uma está ativa    
 
-    },1000)
-    
+        intervalo=setInterval(()=>{
+            segundos++
+            if(segundos===60){
+                minutos++
+                segundos=0
+            }
+            if(minutos===60){
+                horas++
+                minutos=0
+            }
+            
+            const hrsStr=horas.toString().padStart(2,'0')
+            const minStr=minutos.toString().padStart(2,'0')
+            const segStr=segundos.toString().padStart(2,'0')
+            crono.innerHTML=`${hrsStr}:${minStr}:${segStr}`
+            
+        },1000) 
+    contando=true//impede outra contagem junto com o if
 })
 
-parar.addEventListener("click",()=>{
-    crono.innerHTML=0
+pausar.addEventListener("click",()=>{
     clearInterval(intervalo)
+    contando=false
+
+})
+
+
+parar.addEventListener("click",()=>{    
+    clearInterval(intervalo)
+    contando=false
+    intervalo=null
+    segundos=0
+    minutos=0
+    horas=0
+    crono.innerHTML="00:00:00"
+
 })
