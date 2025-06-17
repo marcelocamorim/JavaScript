8938f26816ee9c3d11913d3f62af06b004ad3d09
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-17 20:10:19 -0300

    praticando exercicios

--- exercicios/ex_datas03.js
+++ exercicios/ex_datas03.js
@@ -15,50 +15,68 @@ Quando for igual, toque um som (ou mostre uma mensagem se não quiser usar áudi
 
  */
 
-
+//
 const div_relogio = document.getElementById("div_relogio")
 const input_alarme = document.getElementById("input_alarme")
 const hora_do_alarme = document.getElementById('hora_do_alarme')
 
-const ativar=document.getElementById('ativar')
-const parar=document.getElementById('parar')
+const ativar = document.getElementById('ativar')
+const parar = document.getElementById('parar')
 
-alarme_ativado=false
+const somAlarme = new Audio('alarme.mp3')
+
+let alarmeAtivado=false
+let alarmeTocando=false
+let horaAlvo=""
+let horaCompleta=""
+
+
+
+const relogio=()=>{
+    let data=new Date()
+
+    let hora=data.getHours()
+    hora=hora<10?"0"+hora:hora
+
+    let minutos=data.getMinutes()
+    minutos=minutos<10?"0"+minutos:minutos
+
+    let segundos=data.getSeconds()
+    segundos=segundos<10?"0"+segundos:segundos
+
+    horaCompleta=`${hora}:${minutos}:${segundos}`
+
+    div_relogio.innerHTML=horaCompleta
+
+    if(alarmeAtivado && horaCompleta===horaAlvo && !alarmeTocando){
+        alarmeTocando=true
+        somAlarme.play()
+        somAlarme.loop=true
+        hora_do_alarme.innerHTML="Alarme Tocando"
+    }
+}
+
+let intervalo=setInterval(relogio,1000)
+
+hora_do_alarme.innerHTML="Defina a Hora do Alarme"
 
 
 ativar.addEventListener("click",()=>{
-    
-    
+    alarmeAtivado=true
+    horaAlvo=input_alarme.value
+    hora_do_alarme.innerHTML=`Alarme definido para: ${horaAlvo}`
+
 })
-
-
-
 
 parar.addEventListener("click",()=>{
-
+    alarmeAtivado=false
+    alarmeTocando=false
+    somAlarme.pause()
+    somAlarme.currentTime=0
+    hora_do_alarme.innerHTML="Alarme Parado ou não definido"
 })
 
 
-
-const relogio = () => {
-    const data = new Date()
-
-    let hora = data.getHours()
-    hora = hora < 10 ? "0" + hora : hora
-
-    let minutos = data.getMinutes()
-    minutos = minutos < 10 ? "0" + minutos : minutos
-
-    let segundos = data.getSeconds()
-    segundos = segundos < 10 ? "0" + segundos : segundos
-
-    let hora_completa = `${hora}:${minutos}:${segundos}`
-
-    div_relogio.innerHTML = hora_completa
-
-
-}
-const intervalo = setInterval(relogio, 1000)
 
 
 
