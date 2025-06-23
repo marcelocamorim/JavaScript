dcd8855b83e8afd19694af0efa55c5f2d0994fb0
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-06-23 18:45:10 -0300

    Update aula81.js

--- CFB cursos/aula81.js
+++ CFB cursos/aula81.js
@@ -12,12 +12,10 @@ window.addEventListener("mousemove", (evt) => {
     const posx_mouse = evt.clientX
     const posy_mouse = evt.clientY
 
-    olhos.forEach(ol => {
-        const rect = ol.getBoundingClientRect()
-        const olhoX = rect.left + rect.width / 2
-        const olhoY = rect.top + rect.height / 2
+    // Exemplo: ângulo baseado sempre no canto superior esquerdo (0,0)
+    const angle = Math.atan2(posy_mouse, posx_mouse) * 180 / Math.PI
 
-        const angle = Math.atan2(posy_mouse - olhoY, posx_mouse - olhoX) * 180 / Math.PI
+    olhos.forEach(ol => {
         ol.style.transform = `rotate(${angle}deg)`
     })
 })
