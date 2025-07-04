3c9bf70b0b854aca3220644443a0fa1dbfdf90f6
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-07-04 12:53:24 -0300

    Create aula94.js

---
+++ CFB cursos/aula94.js
@@ -0,0 +1,8 @@
+//para importar com "Export Default" necessário ficar fora das "{}" chaves
+import getTodosCursos, { cursos, getCurso } from "./aula94-cursos.js";
+//function fora das chaves, array dentro das chaves
+
+
+console.log(cursos)
+console.log(getTodosCursos())
+console.log(getCurso(1))
