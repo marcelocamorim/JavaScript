d7aedc6126fa026dc24660faa823eb98691a2983
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-07-04 11:55:22 -0300

    Create aula92-cursos.js

---
+++ CFB cursos/aula92-cursos.js
@@ -0,0 +1,7 @@
+//Código que será exportado para outro documento
+const cursos=["JavaScript","HTML","CSS","Arduino","Raspbarry","C++","Python","?Java","C#"]
+const carros=["Polo","T-Crross","Golf","Onix","Cruze","Argo","Cronos"]
+
+
+//exporta para outro documento
+export {cursos,carros}
