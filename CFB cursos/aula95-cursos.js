c411963f3e193d140f10e7033885e2e1962d035d
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-07-04 13:14:23 -0300

    Create aula95-cursos.js

---
+++ CFB cursos/aula95-cursos.js
@@ -0,0 +1,22 @@
+class Cursos{
+    static cursos=["JavaScript","HTML","CSS","Arduino","Raspbarry","C++","Python","?Java","C#"]
+
+    static getTodosCursos=()=>{
+        return this.cursos
+    }
+
+    static getCurso=(i_curso)=>{
+        return this.cursos[i_curso]
+    }
+
+    static addCurso=(novoCurso)=>{
+        this.cursos.push(novoCurso)
+    }
+
+    static apagarCursos=()=>{
+        this.curso=[]
+    }
+}
+
+export{Cursos}
+
