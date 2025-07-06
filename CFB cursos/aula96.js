154ee7d008f9e278b1c01881e15d8fe2d9bbba1f
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-07-06 11:49:39 -0300

    Update aula96.js

---
+++ CFB cursos/aula96.js
@@ -0,0 +1,13 @@
+import c from "./aula96_contatos.js";
+
+const listaContatos=document.getElementById("listaContatos")
+const btn_gravar=document.getElementById("btn_gravar")
+
+btn_gravar.addEventListener("click",()=>{
+    const cont={
+        nome:document.getElementById("f_nome").value,
+        telefone:document.getElementById("f_telefone").value,
+        email:document.getElementById("f_email").value
+    }
+    c.addContato
+})
