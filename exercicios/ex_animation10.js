308f8f7599d5d994bd6f811898028e9bb0588f84
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-08-22 21:46:44 -0300

    Create ex_animation10.js

---
+++ exercicios/ex_animation10.js
@@ -0,0 +1,87 @@
+// Exercício 10: Pista Limitada
+
+// Crie uma "pista" (por exemplo, uma <div> com largura e altura definidas) e faça o carro se mover apenas dentro dela.
+
+// Assim você pratica o uso de offsetLeft, offsetTop, offsetWidth, offsetHeight.
+
+const carro = document.getElementById("carro")
+const btn_parar = document.getElementById("btn_parar")
+const btn_esquerda = document.getElementById("btn_esquerda")
+const btn_direita = document.getElementById("btn_direita")
+const painel = document.getElementById("painel")
+
+const btn_play = document.createElement('button')
+btn_play.innerText = "Play"
+painel.appendChild(btn_play)
+
+const btn_reset = document.createElement('button')
+btn_reset.innerText = "Reset"
+painel.appendChild(btn_reset)
+
+
+
+let anima = null
+let limiteHorizontal = null
+let limiteVertical = null
+let direcao = 1
+
+const init = () => {
+    carro.style = `position: relative; left:0px; top:0px;`
+    attLimites()
+
+}
+
+const attLimites = () => {
+    limiteHorizontal = window.innerWidth - carro.offsetWidth
+    limiteVertical = window.innerHeight - carro.offsetHeight
+}
+
+const move = (horizon, vert) => {
+    let posH = parseInt(carro.style.left)
+    let posV = parseInt(carro.style.top)
+
+    let newH = posH + (10 * direcao)
+    let newV = posV + (10 * direcao)
+
+
+
+    if (newH >= limiteHorizontal) {
+        newH = limiteHorizontal
+        direcao = -1
+    } else if (newH <= 0) {
+        newH = 0
+        direcao = 1
+    }
+
+
+    carro.style.left = newH + "px"
+    console.log(newH)
+
+
+}
+
+const startMove = (h, v) => {
+    clearInterval(anima)
+    anima = setInterval(move, 20, h, v)
+}
+
+
+btn_play.addEventListener("click", () => {
+    startMove()
+})
+
+
+btn_parar.addEventListener("click", () => {//Stop
+    clearInterval(anima)
+})
+
+btn_reset.addEventListener("click",()=>{
+    clearInterval(anima)
+    carro.style.left="0px"
+    carro.style.top="0px"
+})
+
+
+window.addEventListener("load", init)
+
+window.addEventListener("resize", attLimites)
