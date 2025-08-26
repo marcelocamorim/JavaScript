0d62791118f55d1f17d34e828476dfc13c42edfd
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-08-26 19:55:02 -0300

    Update aula120.js

--- CFB cursos/aula120.js
+++ CFB cursos/aula120.js
@@ -5,14 +5,19 @@ const f_nota=document.querySelector("#f_nota")
 const f_msg=document.querySelector("#f_msg")
 
 document.querySelector("#btn_validar").addEventListener("click",(evt)=>{
-    let msg=null
+    let estadoValidacao=f_nota.validity
 
-    if(!f_nota.checkValidity()){
-        msg=f_nota.validationMessage
+    if(estadoValidacao.valueMissing){
+        f_nota.setCustomValidity("Esse Campo é Obrigatório")
+    }else if(estadoValidacao.rangeOverflow){
+        f_nota.setCustomValidity("Essa nota é muito alta")
+    }else if(estadoValidacao.rangeUnderflow){
+        f_nota.setCustomValidity("Essa nota é muito baixa")
     }
 
-    f_msg.innerHTML=msg
+    //f_nota.reportValidity()
     evt.preventDefault()
+    f_msg.innerHTML=f_nota.validationMessage
     
 })
 
@@ -21,3 +26,25 @@ document.querySelector("#btn_validar").addEventListener("click",(evt)=>{
 // O script confere se o campo f_nota atende às regras de validação do HTML (required, min, max etc.).
 // Se não atender, mostra a mensagem de erro do próprio navegador em #f_msg.
 // Sempre impede o envio do formulário (para não recarregar a página).
+
+
+//Métodos de validação do DOM
+//checkValidity()
+//setCustomValidity()
+
+//Propriedade de validação do DOM
+//validity
+//validationMessage
+
+//Propriedades de validação
+/*
+customError: true, se uma mensagem de validação personalizada for definida.
+patternMismatch: true, se o valor de um elemento não corresponder ao seu atributo padrão.
+rangeOverflow: true, se o valor de um elemento for maior que seu atributo max.
+rangeUnderflow: true, se o valor de um elemento for menor que seu atributo min.
+stepMismatch: true, se o valor de um elemento for inválido por seu atributo step.
+tooLong: true, se o valor de um elemento exceder seu atributo maxLength.
+typeMismatch: true, se o valor de um elemento for inválido por seu atributo type.
+valueMissing: true, se um elemento (com um atributo obrigatório) não tiver valor.
+valid: true, se o valor de um elemento for válido.
+*/
