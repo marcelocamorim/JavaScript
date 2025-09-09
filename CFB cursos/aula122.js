747fbb6a674beb255c1e624d8259c8ac18b8f962
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-09 19:26:54 -0300

    Update aula122.js

--- CFB cursos/aula122.js
+++ CFB cursos/aula122.js
@@ -1,5 +1,8 @@
 //Aprendendo sobre FETCH para consumo de APIs em Javascript #P1 - Curso de Javascript - Aula 122
 
-const endPoint="https://cfbcursos.repl.co/"
-fetch(endPoint)
-.then(res=>console.log(res))
+const endpoint="https://cfbcursos.repl.co/"
+fetch(endpoint)
+.then(res=>res.json())
+.then(dados=>{
+    console.log(dados)
+})
