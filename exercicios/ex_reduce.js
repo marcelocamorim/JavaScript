521c907322a213009f168677db0a3ef5988c390e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-18 12:38:28 -0300

    Update ex_reduce.js

--- exercicios/ex_reduce.js
+++ exercicios/ex_reduce.js
@@ -8,18 +8,18 @@ const somar = () => {
     let soma = numero.reduce((acc, val) => acc + val, 0)
     return soma
 }
-console.log(somar())
+console.log("ex01: "+somar())
 
-// Multiplicar todos os números
+//ex02 Multiplicar todos os números
 // Faça uma função que multiplique todos os valores de um array e retorne o resultado.
 const multiplicar = () => {
     let numeros = [2, 2, 10]
     let mult = numeros.reduce((ac, val) => ac * val)
     return mult
 }
-console.log(multiplicar())
+console.log("ex02: "+multiplicar())
 
-//Encontrar o maior valor
+//ex03 Encontrar o maior valor
 // Use reduce para encontrar o maior número dentro de um array.
 
 let numeros2 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -28,16 +28,16 @@ const maior = (arrray) => {
         return prev > curr ? prev : curr
     })
 }
-console.log(maior(numeros2))
+console.log("ex03: "+maior(numeros2))
 
 // 4 Contar elementos repetidos
 // Dado um array de strings, conte quantas vezes cada palavra aparece.
 let frutas = ["uva", "banana", "uva", "abacaxi", "uva", "banana"]
-let contagem = frutas.reduce((acc, fruta) => {
-    acc[fruta] = (acc[fruta] || 0) + 1
+let contagem=frutas.reduce((acc, fruta)=>{
+    acc[fruta]= (acc[fruta] || 0) +1
     return acc
-}, {})
-console.log(contagem)
+},{})
+console.log("ex 04: ", contagem)
 
 //5 Somar apenas números pares
 // Some apenas os valores pares de um array.
@@ -51,9 +51,9 @@ let numeros3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 //         }
 //     },0)
 //     return soma
-const somarPares = () =>
-    numeros3.reduce((acc, val) => acc + (val % 2 === 0 ? val : 0), 0)
-console.log(somarPares())
+const somarPares=()=>
+    numeros3.reduce((acc,val)=> acc + (val%2===0?val:0),0)
+console.log("ex05: "+somarPares())
 
 
 //6 Transformar array em string
@@ -62,21 +62,21 @@ let palavras=["praticando", "exercicios", "de", "reduce"]
 
 let frase=palavras.reduce((prev, curr)=> prev + " " + curr)
 //let frase=palavras.join(" ")
-console.log(frase)
+console.log("ex06: "+frase)
 
 //7 Flatten (achatar arrays)
 // Dado um array de arrays, transforme em um único array.
 let arrayDeArrays=[["achatando", "arrays"], ["com", "reduce"], [1,2,3]]
 let novoArray=arrayDeArrays.reduce((acc,val)=> acc.concat(val), [])
-console.log(novoArray)
+console.log("ex07: " +novoArray)
 
 //8 Calcular a média dos números
 // Faça uma função que retorne a média dos valores de um array.
 let notas=[2,10,8,5,5]
 const calcularMedia=()=>{
-    return notas.reduce((acc,val)=>acc+val/notas.length,0)
+    return notas.reduce((acc,val)=>acc+val/notas.length,0)  
 }
-console.log(calcularMedia())
+console.log("ex08: "+  calcularMedia())
 
 //9 Agrupar por categoria
 // Dado um array de objetos, agrupe-os por uma propriedade.
