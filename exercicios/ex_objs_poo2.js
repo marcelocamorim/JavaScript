34ea4d21f3c6224b6dba8629b7d176618bbd640e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-20 10:13:00 -0300

    Create ex_objs_poo2.js

---
+++ exercicios/ex_objs_poo2.js
@@ -0,0 +1,7 @@
+// 🔹 Intermediários
+
+// Crie um objeto pessoa com nome, idade e um método chamado apresentar() que retorne uma frase com essas informações.
+
+// Crie um objeto cachorro com propriedades nome e raca. Depois adicione dinamicamente a propriedade idade.
+
+// Crie um objeto usuario com nome e email. Depois adicione uma função alterarEmail(novoEmail) que muda o email do usuário.
