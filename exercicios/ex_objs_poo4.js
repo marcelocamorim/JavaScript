85dcac7f8d04d3072190b3295e5fb5eee579e232
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-20 10:13:04 -0300

    Create ex_objs_poo4.js

---
+++ exercicios/ex_objs_poo4.js
@@ -0,0 +1,7 @@
+// 🔹 Avançados
+
+// Crie um objeto contaBancaria com propriedades saldo e métodos depositar(valor) e sacar(valor). Teste as operações.
+
+// Crie um objeto carrinho que começa vazio. Adicione dinamicamente produtos com nome e preco, e depois calcule o total da compra.
+
+// Crie um objeto biblioteca que contém um array de livros (cada livro é um objeto com título e autor). Mostre todos os títulos dos livros usando um loop.
