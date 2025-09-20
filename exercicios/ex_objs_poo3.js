6afac5a949e248adb167a5dc6379035ca9462009
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-20 10:13:02 -0300

    Create ex_objs_poo3.js

---
+++ exercicios/ex_objs_poo3.js
@@ -0,0 +1,7 @@
+// 🔹 Percorrendo Objetos
+
+// Crie um objeto produtos com vários itens e preços. Use for...in para mostrar o nome do produto e o preço.
+
+// Crie um objeto pessoa com várias informações (nome, idade, cidade). Use Object.keys() para listar as chaves e Object.values() para listar os valores.
+
+// Crie um array de objetos alunos com nome e nota. Use for...of para percorrer e mostrar apenas os alunos com nota maior ou igual a 7.
