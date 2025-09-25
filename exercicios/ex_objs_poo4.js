ff984f2d2e9c41549de0867be9fbd28d4ac75255
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-09-24 22:12:31 -0300

    Update ex_objs_poo4.js

--- exercicios/ex_objs_poo4.js
+++ exercicios/ex_objs_poo4.js
@@ -1,7 +1,35 @@
 // 🔹 Avançados
 
-// Crie um objeto contaBancaria com propriedades saldo e métodos depositar(valor) e sacar(valor). Teste as operações.
 
 // Crie um objeto carrinho que começa vazio. Adicione dinamicamente produtos com nome e preco, e depois calcule o total da compra.
 
 // Crie um objeto biblioteca que contém um array de livros (cada livro é um objeto com título e autor). Mostre todos os títulos dos livros usando um loop.
+
+
+
+// Crie um objeto contaBancaria com propriedades saldo e métodos depositar(valor) e sacar(valor). Teste as operações.
+
+let contaBancaria={
+    saldo:0,
+
+    depositar: function(valor){
+        this.saldo+=valor
+    },
+
+    sacar: function(valor){
+        if(valor <= this.saldo){
+            this.saldo -= valor
+        }else{
+            console.log("Saldo insuficiente para essa transação")
+        }
+    }
+}
+
+contaBancaria.depositar(100)
+contaBancaria.depositar(100)
+
+contaBancaria.sacar(55)
+contaBancaria.sacar(55)
+contaBancaria.sacar(55)
+contaBancaria.sacar(55)
+console.log(contaBancaria.saldo)
