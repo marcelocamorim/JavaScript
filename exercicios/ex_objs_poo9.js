b6b24652e0eab9c58e511a2f17957d44b85d1654
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-05 17:11:48 -0300

    Create ex_objs_poo9.js

---
+++ exercicios/ex_objs_poo9.js
@@ -0,0 +1,6 @@
+// 🔹 5. Cadastro de alunos
+// Crie um objeto escola que armazena alunos em um array, cada aluno sendo {nome, notas: []}.
+// Métodos:
+// adicionarAluno(nome)
+// adicionarNota(nome, nota)
+// calcularMedia(nome)
