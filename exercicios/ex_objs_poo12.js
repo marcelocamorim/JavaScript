13c5a1768ec7bc3d559eee36dc434e21534982ac
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-13 10:40:44 -0300

    Update ex_objs_poo12.js

--- exercicios/ex_objs_poo12.js
+++ exercicios/ex_objs_poo12.js
@@ -15,9 +15,12 @@
 let conta = {
     saldo: 0,
     historico: [],
+    
 
     depositar: function (valor) {
+        let agora=new Date
         this.saldo += valor
+        this.historico.push({ tipo: "Depósito", valor: valor, data: agora.toLocaleString("pt-br", {dateStyle: "short", timeStyle: "short"}) })
         console.log(`Deposito de ${valor} Realizado com Sucesso - Saldo atual ${this.saldo}`)
 
     },
@@ -27,25 +30,26 @@ let conta = {
             console.log(`Saldo insuficiente para essa transação - Saldo em conta ${this.saldo}`)
             return
         }
-
+        let agora=new Date
         this.saldo -= valor
+        this.historico.push({ tipo: "Saque", valor: valor, data: agora.toLocaleString("pt-br", {dateStyle: "short", timeStyle: "short"}) })
         console.log(`Saque de ${valor} Realizado com Sucesso - Saldo atual: ${this.saldo}`)
 
     },
 
     exibirExtrato: function () {
-        let agora = new Date
-        if(this.depositar()){
-            //this.historico.push({tipo: "Depósito", valor:})
-            console.log("testeeee")
-        }
-        
-        
+        console.log(`Histótico de transações:`)
+        this.historico.forEach((el)=>{
+            console.log(`Operação: ${el.tipo} - Valor: ${el.valor} - Data: ${el.data}`)
+        })
+        console.log(`Saldo Atual: ${this.saldo}`)
     }
 }
 
 
 
-conta.exibirExtrato()
-//conta.depositar(100)
+
+conta.depositar(100)
 conta.sacar(20)
+conta.exibirExtrato()
+
