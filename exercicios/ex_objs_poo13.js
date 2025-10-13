95b8ba7b87c8a94b0c781af25274bf5eeeb30ad8
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-13 11:45:12 -0300

    Update ex_objs_poo13.js

--- exercicios/ex_objs_poo13.js
+++ exercicios/ex_objs_poo13.js
@@ -8,3 +8,33 @@
 // mostrarPedidos() → lista todos os pratos e quantidades.
 // 🧠 Desafio extra: evite pedidos duplicados (se já existir, apenas aumente a quantidade).
 
+let restaurante = {
+    cardapio: [
+        { nome: "pizza", preco: 80 },
+        { nome: "Refrigerante", preco: 10 },
+        { nome: "sorvete", preco: 15 },
+        { nome: "fritas", preco: 12 },
+    ],
+
+    pedidos: [],
+
+
+    fazerPedido: function (nomePrato, quantidade) {
+        let prato = this.cardapio.find((p => p.nome === nomePrato))
+        if(!prato){
+            console.log(`Item ${nomePrato} indisponivel no cardapio`)
+            return
+        }
+        if(this.pedidos[!prato]){
+            let valorTotal = prato.preco * quantidade
+            this.pedidos.push({ nome: nomePrato, quantidade: quantidade, total:valorTotal})
+        }
+
+    }
+
+}
+
+restaurante.fazerPedido("atum",2)
+restaurante.fazerPedido("pizza",2)
+console.log(restaurante.pedidos)
+
