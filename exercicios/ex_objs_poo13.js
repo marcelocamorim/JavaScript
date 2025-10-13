6922109156aa022cb3b428820fcf3b771d0caa67
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-13 17:23:22 -0300

    Update ex_objs_poo13.js

--- exercicios/ex_objs_poo13.js
+++ exercicios/ex_objs_poo13.js
@@ -11,30 +11,57 @@
 let restaurante = {
     cardapio: [
         { nome: "pizza", preco: 80 },
-        { nome: "Refrigerante", preco: 10 },
+        { nome: "refrigerante", preco: 10 },
         { nome: "sorvete", preco: 15 },
         { nome: "fritas", preco: 12 },
     ],
 
+        //nome, quantidade, total 
     pedidos: [],
-
+    
 
     fazerPedido: function (nomePrato, quantidade) {
         let prato = this.cardapio.find((p => p.nome === nomePrato))
-        if(!prato){
+        if (!prato) {
             console.log(`Item ${nomePrato} indisponivel no cardapio`)
             return
         }
-        if(this.pedidos[!prato]){
-            let valorTotal = prato.preco * quantidade
-            this.pedidos.push({ nome: nomePrato, quantidade: quantidade, total:valorTotal})
+        let valorTotal = prato.preco * quantidade
+
+
+
+        let pedidoExistente = this.pedidos.find((pe => pe.nome === nomePrato))
+        if (!pedidoExistente) {
+            this.pedidos.push({ nome: nomePrato, quantidade: quantidade, total: valorTotal })
+            console.log(`${quantidade} - ${nomePrato} Adicionado ao pedido`)
+            return
+        } else {
+            pedidoExistente.quantidade += quantidade
+            pedidoExistente.total += valorTotal
+            console.log(`Quantidade de ${nomePrato} Atualizada para ${pedidoExistente.quantidade}`)
         }
 
-    }
+    },
 
+    calcularTotal:function(){
+        let total=this.pedidos.reduce((acc,item)=>acc+item.total,0)
+        console.log(`Total R$${total}`)
+        return total
+    },
+
+    mostraPedidos:function(){
+        console.log(`Resumo do Pedido:`)
+        this.pedidos.forEach((el)=>{
+            console.log(`Item: ${el.nome} - Quantidade ${el.quantidade}`)
+        })
+    }
 }
 
-restaurante.fazerPedido("atum",2)
-restaurante.fazerPedido("pizza",2)
-console.log(restaurante.pedidos)
+restaurante.fazerPedido("atum", 2)
+restaurante.fazerPedido("pizza", 2)
+restaurante.fazerPedido("pizza", 2)
+restaurante.fazerPedido("refrigerante", 2)
+restaurante.mostraPedidos()
+restaurante.calcularTotal()
+
 
