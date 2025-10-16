888c0d79c2df9cfeb205f2e4b137fd477ad3087c
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-15 21:00:30 -0300

    Create ex_class08.js

---
+++ exercicios/ex_class08.js
@@ -0,0 +1,7 @@
+// 🔹 7. Classe Loja
+// Crie uma classe Loja que contenha um array de produtos (objetos da classe Produto).
+// Métodos:
+// adicionarProduto(produto)
+// listarProdutos()
+// buscarProduto(nome) → retorna o produto se existir
+// 🧠 Desafio extra: use find() e filter() nos métodos de busca.
