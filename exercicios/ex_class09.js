3d6b3c8c5dc58223155560be6ef775000f907495
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-15 21:00:33 -0300

    Create ex_class09.js

---
+++ exercicios/ex_class09.js
@@ -0,0 +1,10 @@
+// 🧠 PARTE 4 — Projeto Integrador
+// 🔹 8. Sistema de Escola
+// Crie:
+// Uma classe Aluno com nome e notas (array).
+// Uma classe Turma que armazena vários alunos.
+// Métodos da Turma:
+// adicionarAluno(aluno)
+// calcularMediaGeral() → média de todos os alunos.
+// melhoresAlunos() → retorna os com média >= 7.
+// 🧠 Desafio extra: adicione um método removerAluno(nome).
