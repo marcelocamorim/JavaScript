4c80ca9bfdc0041fd2626c6decac1f4f51e87e87
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-20 11:10:35 -0300

    Update ex_objs_poo19.js

--- exercicios/ex_objs_poo19.js
+++ exercicios/ex_objs_poo19.js
@@ -14,53 +14,60 @@
 // Desafio extra: permita adicionar e remover itens dinamicamente.
 
 
-
 class Produto {
     constructor(nome, preco) {
         this.nome = nome
         this.preco = preco
     }
-
 }
 
 class ItemPedido {
-    constructor(produto, quantidade) {
-        this.produto = produto
-        this.quantidade = quantidade
+    constructor(produto, quantidade){
+        this.produto=produto
+        this.quantidade=quantidade
     }
 
-    subtotal() {
+    subtotal(){
         return this.produto.preco * this.quantidade
     }
-
 }
 
-class Pedido {
-    constructor(cliente) {
-        this.cliente = cliente
-        this.itens = []
+class Pedido{
+    constructor(cliente){
+        this.cliente=cliente
+        this.itens=[]
     }
 
-    adicionarItem(item) {
+    adicionarPedido(item){
         this.itens.push(item)
     }
 
-    calcularTotal() {        
-        return this.itens.reduce((total, item) => total + item.subtotal(), 0)
+    calcularTotal(){
+        return this.itens.reduce((total,item)=>total+item.subtotal(),0)
+    }
+
+    info(){
+        console.log(`Pedido do cliente ${this.cliente}`)
+        this.itens.forEach((el)=>{
+            console.log(`Item: ${el.produto.nome} - Preço: R$${el.produto.preco} - Quantidade:${el.quantidade} - Total: ${el.subtotal().toFixed(2)}`)
+        })
+        console.log(`Total do pedido R$${this.calcularTotal().toFixed(2)}`)
     }
 }
 
-const produto1 = new Produto("pizza", 70)
-const produto2 = new Produto("pastel", 15)
+const produto1=new Produto("notebook", 1000)
+const produto2=new Produto("teclado", 100)
+const produto3=new Produto("mouse", 50)
 
-const item1 = new ItemPedido(produto1, 2)
+const item1 = new ItemPedido(produto1, 1)
+const item2 = new ItemPedido(produto2, 1)
+const item3 = new ItemPedido(produto3, 2)
 
-const pedido = new Pedido("João")
-pedido.adicionarItem(item1)
+const pedido1=new Pedido("João")
+pedido1.adicionarPedido(item1)
+pedido1.adicionarPedido(item2)
+pedido1.adicionarPedido(item3)
 
-
-
-
-
+pedido1.info()
 
 
