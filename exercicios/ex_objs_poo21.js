54e25a54e88cfbc9bbe7fc431e6011247368264e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-22 18:26:38 -0300

    Update ex_objs_poo21.js

--- exercicios/ex_objs_poo21.js
+++ exercicios/ex_objs_poo21.js
@@ -12,39 +12,108 @@
 // Desafio extra:
 // Adicione um método aplicarDesconto(percentual) que reduz o total.
 
-class Produto{
-    constructor(nome, preco){
-        this.nome=nome
-        this.preco=preco
+class Produto {
+    constructor(nome, preco) {
+        this.nome = nome
+        this.preco = preco
+        this.quantidade=null
+
     }
 }
 
-class Carrinho{
-    constructor(){
-        this.carrinho=[]
+class Carrinho {
+    constructor() {
+        this.carrinho = []
     }
 
-    adicionarItem(item){
-        this.carrinho.push(item)
+    separarInfo(){
+        console.log("------------------------")
     }
 
-    removerProduto(produtoNome){
-        this.carrinho=this.carrinho.filter(p=>p.nome!==produtoNome)
+    adicionarItem(item, quantidade) {
+        const itemExistente = this.carrinho.find((i => i.nome === item.nome))
+
+        if (!itemExistente) {
+            console.log(`item ${item.nome} Adicionado ao carrinho!`)
+            item.quantidade=quantidade
+            this.carrinho.push(item)
+        } else {
+            console.log(`item ${item.nome} - quantidade atualizada!`)
+            itemExistente.quantidade += quantidade
+        }
+        this.separarInfo()
+    }
+
+    removerProduto(produtoNome, quantidade) {
+        const itemExistente=this.carrinho.find((el=>el.nome===produtoNome))
+
+        if(!itemExistente){
+            console.log(`${produtoNome} Não consta no carrinho`)
+            this.separarInfo()
+            return
+
+        }
+
+        if(quantidade>=itemExistente.quantidade){
+            this.carrinho = this.carrinho.filter(p => p.nome !== produtoNome)
+            console.log(`Item: ${produtoNome} Removido do Carrinho!`)
+            this.separarInfo()
+        }
+
+        if(quantidade<itemExistente.quantidade){
+            itemExistente.quantidade -= quantidade
+            console.log(`${quantidade} ${produtoNome} Removido - quantidade Atualizada`)
+            this.separarInfo()
+        }
+    }
+
+   
+    calcularTotal(){
+        let total=this.carrinho.reduce((acc,item)=>acc+(item.preco*item.quantidade),0)
+        console.log(`Total R$${total.toFixed(2)}`)
+        this.separarInfo()
+        return total
+    }
+
+    resumoPedido(){
+        console.log("Resumo do Pedido")
+        this.carrinho.forEach((el)=>{
+            console.log(`Item: ${el.nome} - Preço: R$${el.preco} - Quantidade: ${el.quantidade}`)
+        })
+
+        this.calcularTotal()
+
+    }
+
+    adicionarDesconto(porcentagem){
+        let total=this.calcularTotal()        
+        let totalComDesconto= total - ((porcentagem/100)*total)
+        console.log(`Desconto de ${porcentagem}% Aplicado! - Total: R$${totalComDesconto}`)
+
+
     }
 }
 
-const produto1=new Produto("teclado",120)
-const produto2=new Produto("monitor",900)
-const produto3=new Produto("mouse",40)
-const produto4=new Produto("memoria ram",310)
+const produto1 = new Produto("teclado", 120)
+const produto2 = new Produto("monitor", 900)
+const produto3 = new Produto("mouse", 40)
+const produto4 = new Produto("memoria ram", 310)
 
-const carrinho1=new Carrinho()
-carrinho1.adicionarItem(produto1)
-carrinho1.adicionarItem(produto2)
-carrinho1.adicionarItem(produto3)
-carrinho1.adicionarItem(produto4)
+const carrinho1 = new Carrinho()
+carrinho1.adicionarItem(produto1, 1)
+carrinho1.adicionarItem(produto1, 1)
+carrinho1.adicionarItem(produto1, 1)
+carrinho1.adicionarItem(produto1, 1)
+carrinho1.adicionarItem(produto2,1)
+carrinho1.adicionarItem(produto3,1)
+carrinho1.adicionarItem(produto4,1)
 
-carrinho1.removerProduto("teclado")
+// carrinho1.removerProduto("teclado", 1)
+// carrinho1.removerProduto("teclado", 1)
+// carrinho1.removerProduto("teclado", 1)
+// carrinho1.removerProduto("teclado", 1)
 
-console.log(carrinho1.carrinho)
+carrinho1.resumoPedido()
+carrinho1.adicionarDesconto(10)
+
 
