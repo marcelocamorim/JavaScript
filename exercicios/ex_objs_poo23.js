6c048388d0c789510423374b82668f049dce3a2c
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-24 18:51:55 -0300

    Update ex_objs_poo23.js

--- exercicios/ex_objs_poo23.js
+++ exercicios/ex_objs_poo23.js
@@ -14,81 +14,117 @@
 // Permita adicionar e remover itens do pedido antes de fechar.
 
 
-class Prato{
-    constructor(nome, preco){
-        this.nome=nome
-        this.preco=preco
+class Prato {
+    constructor(nome, preco) {
+        this.nome = nome
+        this.preco = preco
     }
 }
 
-class Pedido{
-    constructor(prato, quantidade){
-        this.prato=prato
-        this.quantidade=quantidade        
+class Pedido {
+    constructor(prato, quantidade) {
+        this.prato = prato
+        this.quantidade = quantidade
     }
 
-    subtotal(){
+    subtotal() {
         return this.prato.preco * this.quantidade
     }
 }
 
-class Mesa{
-    constructor(){        
-        this.clientes=[]
-        this.pedidos=[]
+class Mesa {
+    constructor() {
+        this.clientes = []
+        this.pedidos = []
     }
 
-    registrarCliente(clienteNome){
+    registrarCliente(clienteNome) {
         this.clientes.push(clienteNome)
     }
 
-    fazerPedido(clienteNome, prato, quantidade){
-        const pedido=new Pedido(prato, quantidade)
-        this.pedidos.push(clienteNome, pedido)
+    fazerPedido(clienteNome, prato, quantidade) {
+        const pedido = new Pedido(prato, quantidade)
+        this.pedidos.push({ cliente: clienteNome, pedido })
+    }
+
+    fecharConta() {
+        console.log(`Resumo do pedido`)
+        this.pedidos.forEach((el) => {
+            console.log(`Cliente: ${el.cliente} - Prato: ${el.pedido.prato.nome} - Preço: R$${el.pedido.prato.preco} - Quantidade: ${el.pedido.quantidade} - Subtotal: ${el.pedido.subtotal()}`)
+            console.log("-----------------------------------------------------")
+
+        })
+        const total = this.pedidos.reduce((acc, item) => acc + (item.pedido.subtotal()), 0)
+        console.log(`Total a Pagar: R$${total}`)
+        return total
     }
 }
 
 
-class Restaurante{
-    constructor(){
-        this.mesas=[]
+class Restaurante {
+    constructor() {
+        this.mesas = []
     }
 
-    
-
-    registrarMesa(mesa){
+    registrarMesa(mesa) {
         this.mesas.push(mesa)
     }
 
-    mesasInfo(){
-        this.mesas.forEach((el,i)=>{
-            if(el.clientes.length){
-                console.log(`Mesa ${i+1} Ocupada`)
+    mesasInfo() {
+        this.mesas.forEach((el, i) => {
+            if (el.clientes.length) {
+                console.log(`Mesa ${i + 1} Ocupada`)
                 console.log(`${el.clientes.length} - Clientes registrados:`)
                 console.log(`${el.clientes}`)
                 console.log(`---------------------`)
 
-            }else{
-                console.log(`Mesa ${i+1} Livre`)
+            } else {
+                console.log(`Mesa ${i + 1} Livre`)
             }
         })
+    }   
+
+    receberPagamento(mesa) {
+        let pagamentoIndividual = true
+        let pagamentoUnico = false
+        let dividirIgualmente = false
+
+        if (pagamentoIndividual) {
+            this.mesas.forEach((el) => {
+                el.pedidos.forEach((el2) => {
+                    console.log(`Cliente: ${el2.cliente} - Pagar R$${el2.pedido.subtotal()}`)
+                })
+            })
+            return
+
+        } else if (pagamentoUnico) {
+            mesa.fecharConta()
+            return
+
+        } else if (dividirIgualmente) {
+            let quantidadeClientes = mesa.clientes.length
+            let total = mesa.fecharConta()
+            let totalDividido = total / quantidadeClientes
+
+            console.log(`Valor a Pagar: ${totalDividido.toFixed(2)} em 4x`)
+        }
+
+
     }
 }
 
 
+const prato1 = new Prato("macarrão", 80)
+const prato2 = new Prato("bife acebolado", 50)
+const prato3 = new Prato("strogonof", 120)
+const prato4 = new Prato("salada", 45)
 
+const mesa1 = new Mesa()
+const mesa2 = new Mesa()
+const mesa3 = new Mesa()
+const mesa4 = new Mesa()
 
-const prato1=new Prato("macarrão", 80)
-const prato2=new Prato("bife acebolado", 50)
-const prato3=new Prato("strogonof", 120)
-const prato4=new Prato("salada", 45)
-
-const mesa1= new Mesa()
-const mesa2= new Mesa()
-const mesa3= new Mesa()
-const mesa4= new Mesa()
-
-const restaurante= new Restaurante()
+const restaurante = new Restaurante()
 restaurante.registrarMesa(mesa1)
 restaurante.registrarMesa(mesa2)
 restaurante.registrarMesa(mesa3)
@@ -106,7 +142,8 @@ mesa1.fazerPedido("maria", prato3, 1)
 mesa1.fazerPedido("vitória", prato4, 1)
 
 
-// console.log(mesa1)
 // console.log(mesa1.pedidos)
 
-restaurante.mesasInfo()
+// restaurante.mesasInfo()
+//mesa1.fecharConta()
+restaurante.receberPagamento(mesa1)
