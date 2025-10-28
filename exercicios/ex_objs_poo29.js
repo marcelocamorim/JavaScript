ff6f70da864bb3add3dae7f0121005dc1a1f1ea0
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-28 14:02:23 -0300

    Create ex_objs_poo29.js

---
+++ exercicios/ex_objs_poo29.js
@@ -0,0 +1,11 @@
+// 🚀 Exercícios — Padrão Factory Method (nível intermediário → avançando)
+// Exercício 1 – Fábrica de Veículos
+
+// Crie as classes:
+// Carro (com método dirigir())
+// Moto (com método pilotar())
+
+// Crie uma classe VeiculoFactory com o método criarVeiculo(tipo) que:
+// se tipo for "carro", retorna uma instância de Carro
+// se tipo for "moto", retorna uma instância de Moto
+// Teste criando veículos diferentes e chamando seus métodos específicos.
