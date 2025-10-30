453558efab46f6b494166898b713758e724a79fc
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-10-30 18:25:23 -0300

    Update ex_objs_poo34.js

--- exercicios/ex_objs_poo34.js
+++ exercicios/ex_objs_poo34.js
@@ -13,41 +13,68 @@ Retangulo (método area() → base * altura)
 Crie FormaFactory com método criarForma(tipo, ...parametros) e use switch para retornar o objeto correto.
 Teste cada forma chamando area().
 */
-class Circulo{
-    constructor(raio){
-        this.raio=raio
-    }
-    area(){
-        return (Math.PI * Math.pow(this.raio,2)).toFixed(2)
-    }
-}
-
-class Quadrado{
-    constructor(lado){
-        this.lado=lado
-    }
-    area(){
-        return Math.pow(this.lado,2)
+class Forma {
+    constructor(opcoes) {
+        this.raio = opcoes.raio
+        this.lado = opcoes.lado
+        this.base = opcoes.base
+        this.altura = opcoes.altura
     }
 }
 
-class Retangulo{
-    constructor(base, altura){
-        this.base=base
-        this.altura=altura
+class Circulo extends Forma {
+    constructor(opcoes) {
+        super(opcoes)
     }
-    area(){
+    area() {
+        return (Math.PI * Math.pow(this.raio, 2)).toFixed(2)
+    }
+}
+
+class Quadrado extends Forma{
+    constructor(opcoes) {
+        super(opcoes)
+    }
+    area() {
+        return Math.pow(this.lado, 2)
+    }
+}
+
+class Retangulo extends Forma{
+    constructor(opcoes) {
+        super(opcoes)
+    }
+    area() {
         return this.base * this.altura
     }
 }
 
-class FormaFactory{
+class FormaFactory {
 
-    criarForma(){
-        switch(tipo){
-            case circulo:
-                
+    criarForma(tipo, opcoes={}) {
+        switch (tipo.toLowerCase()) {
+            case "circulo":
+                console.log(`Circulo criado com sucesso!`)
+                return new Circulo(opcoes)
+            case "quadrado":
+                console.log(`quadrado criado com sucesso!`)
+                return new Quadrado(opcoes)
+            case "retangulo":
+                console.log(`retangulo criado com sucesso!`)
+                return new Retangulo(opcoes)
+            default:
+                throw new Error(`Tipo de Forma "${tipo}" Invalido!`)
         }
 
     }
 }
+
+const fabrica=new FormaFactory()
+
+const c1 = fabrica.criarForma("circulo", {raio:5})
+const q1 = fabrica.criarForma("quadrado", {lado:5})
+const r1 = fabrica.criarForma("retangulo", {base:10, altura:4})
+
+console.log(c1.area())
+console.log(q1.area())
+console.log(r1.area())
