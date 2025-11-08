8a1c9178faa4da518c41ea2b630b336f1168e0e8
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-11-08 14:00:51 -0300

    Update ex12.js

--- exercicios/exercicios DOM/ex12/ex12.js
+++ exercicios/exercicios DOM/ex12/ex12.js
@@ -1,4 +1,21 @@
-const input=document.getElementById("input")
-const btn=document.getElementById("btn")
-const crono=document.getElementById("crono")
+const input = document.getElementById("input")
+const btn = document.getElementById("btn")
+const crono = document.getElementById("crono")
+
+btn.addEventListener("click", () => {
+    let tempo = parseInt(input.value)
+    crono.innerHTML = tempo
+
+    let intervalo = setInterval(() => {
+        tempo--
+        crono.innerHTML = tempo
+
+        if (tempo <= 0) {
+            clearInterval(intervalo)
+            crono.innerHTML = `Tempo Zerado `
+        }
+    }, 1000)
+
+})
+
 
