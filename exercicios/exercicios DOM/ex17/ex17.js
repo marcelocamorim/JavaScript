84be12faa749ddf67a5503af63cd04cf36a2249a
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-11-11 12:00:33 -0300

    Update ex17.js

--- exercicios/exercicios DOM/ex17/ex17.js
+++ exercicios/exercicios DOM/ex17/ex17.js
@@ -1,13 +1,21 @@
-const lista = [...document.querySelectorAll("#lista li")]
+const lu=document.getElementById("lista")
+const li = [...document.querySelectorAll("#lista li")]
 const inBusca=document.getElementById("inBusca")
 
 
 inBusca.addEventListener("input",()=>{
-
+    const textoBuscado = inBusca.value.toLowerCase()
+    
+    li.forEach((el)=>{
+        const nome=el.textContent.toLowerCase()
+        if(nome.includes(textoBuscado)){
+            el.style.display="list-item"
+        }else{
+            el.style.display="none"
+        }
+    })
+    
 })
 
-lista.forEach((el)=>{
-    console.log(el.textContent)
-})
 
 
