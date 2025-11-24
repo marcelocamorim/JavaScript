aad825defd5a14f0eac0855f7bcd8330b24c5383
marcelo c. amorim <marceloamorim.dev@gmail.com>
2025-11-24 17:16:36 -0300

    Update ex21.js

--- exercicios/exercicios DOM/ex21/ex21.js
+++ exercicios/exercicios DOM/ex21/ex21.js
@@ -1,6 +1,13 @@
 const menu=document.getElementById("menu")
+const fechaMenu=document.getElementById("fecha-menu")
+
 const containerMenu=document.getElementById("containerMenu")
 
 menu.addEventListener("click",()=>{
-    containerMenu.classList.toggle("mostrar-menu")
+    containerMenu.classList.remove("esconder-menu")
+    containerMenu.classList.add("mostrar-menu")
+})
+fechaMenu.addEventListener("click",()=>{
+    containerMenu.classList.remove("mostrar-menu")
+    containerMenu.classList.add("esconder-menu")
 })
