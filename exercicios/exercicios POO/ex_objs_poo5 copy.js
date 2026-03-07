3538ded1a80824110f1808df51e3da92729c473e
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-03-07 11:14:03 -0300

    revisão ex 5
    praticando

--- exercicios/exercicios POO/ex_objs_poo5 copy.js
+++ exercicios/exercicios POO/ex_objs_poo5 copy.js
@@ -7,6 +7,64 @@
 // buscarContato(nome) (retorna o telefone ou "não encontrado").
 
 
+let agenda = {
+    contatos: [
+        //nome,telefone
+
+    ],
+    separar: function () {
+        console.log("-------------------------------")
+    },
+
+    adicionarContato: function (nom, tel) {
+        this.contatos.push({ nome: nom, telefone: tel })
+        console.log(`Contato "${nom}" Adicionado com sucesso!`)
+        this.separar()
+    },
+
+    listarContatos: function () {
+        console.log(`Lista de contatos`)
+        this.contatos.forEach((el) => {
+            console.log(`Nome: ${el.nome} - Telefone: ${el.telefone}`)
+        })
+        this.separar()
+    },
+
+    buscaPorNome: function (nomeBuscado) {
+        let encontrados = this.contatos.filter(c => c.nome.toLowerCase().includes(nomeBuscado.toLowerCase()))
+
+        if (encontrados.length) {
+            console.log(`Busca por nome - Contatos encontrados:`)
+            encontrados.forEach((el) => {
+                console.log(`nome: ${el.nome} - Telefone: ${el.telefone}`)
+            })
+            return
+        }
+
+        console.log(`Contato nome:"${nomeBuscado}" Não encontrado!`)
+    },
+
+    buscaPorTelefone: function (telBuscado) {
+        console.log(`Busca por Telefone - Contatos encontrados:`)
+        let encontrados = this.contatos.filter(c => String(c.telefone).includes(String(telBuscado)))
+        if (encontrados.length) {
+            encontrados.forEach((el) => {
+                console.log(`nome: ${el.nome} - Telefone: ${el.telefone}`)
+            })
+            return
+        }
+        console.log(`Contato telefone:"${telBuscado}" Não encontrado!`)
+    }
+
+}
 
 
+agenda.adicionarContato("maria", 111111111)
+agenda.adicionarContato("joão", 2222222222)
+agenda.adicionarContato("leticia", 333333333)
+agenda.adicionarContato("marcia", 4444444444)
 
+agenda.listarContatos()
+agenda.buscaPorNome("raimun")
+agenda.buscaPorTelefone(11111)
+agenda.buscaPorTelefone(22)
