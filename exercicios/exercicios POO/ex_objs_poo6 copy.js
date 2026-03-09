c5c777d75b6e6eed5c29e8951ecaca00147dff12
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-03-09 00:20:29 -0300

    revisão
    refazendo exercícios

--- exercicios/exercicios POO/ex_objs_poo6 copy.js
+++ exercicios/exercicios POO/ex_objs_poo6 copy.js
@@ -5,85 +5,50 @@
 // removerProduto(nome, quantidade) (não deixar a quantidade ficar negativa)
 // listarProdutos() (mostrar todos os produtos e quantidades).
 
-let estoque = {
-    produtos: [
-        { nome: "teclado", quantidade: 10 },
-        { nome: "mouse", quantidade: 20 },
-        { nome: "monitor", quantidade: 12 },
-        { nome: "impressora", quantidade: 15 },
-        { nome: "roteador", quantidade: 50 },
+let estoque={
+    produtos:[
+        //nome,quantidade
     ],
 
-    listarProdutos: function () {
-        console.log(`Produtos em Estoque`)
-        this.produtos.forEach((el) => {
-            console.log(`Item: ${el.nome} - Quantidade: ${el.quantidade}`)
+    separar:function(){
+        console.log("----------------------------")
+    },
+
+    adicionarProduto:function(nome,quantidade){
+        const produtoExistente=this.produtos.find(p=>p.nome===nome)
+        if(quantidade<=0){
+            console.log(`ERRO - Quantidade invalida `)
+            console.log(`Falha ao adicionar produto`)
+            this.separar()
+            return
+        }
+        if(produtoExistente){
+            produtoExistente.quantidade+=quantidade
+            console.log(`Produto "${nome} +${quantidade}" - Quantidade atualizada! `)
+            this.separar()
+            return
+        }
+
+        this.produtos.push({nome:nome, quantidade:quantidade})
+        console.log(`Produto "${nome} - ${quantidade}x" Adicionado com Suceso!`)
+        this.separar()
+    },
+
+    removerProduto:function(nome,quantidade){
+
+    },
+
+
+    listarProdutos:function(){
+        console.log(`Lista de Produto:`)
+        this.produtos.forEach((el)=>{
+            console.log(`Produto: ${el.nome} - Quantidade: ${el.quantidade}`)
+            this.separar()
         })
-        console.log("--------------------")
-    },
-
-    adicionarProduto: function (nom, quant) {
-        let produtoExistente = this.produtos.find(p => p.nome === nom)
-
-        if (quant <= 0) {
-            console.log("Falha ao Adicionar Produto - Digite uma Quantidade Válida!")
-            console.log("--------------------")
-            return
-        }
-
-        if (produtoExistente) {
-            produtoExistente.quantidade += quant
-            console.log(`Quantidade de ${produtoExistente.nome} Atualizada +${quant}`)
-            console.log("--------------------")
-        } else {
-            this.produtos.push({ nome: nom, quantidade: quant })
-            console.log(`Produto ${nom} adicionado com Sucesso!`)
-            console.log("--------------------")
-        }
-
-    },
-
-    removerProduto: function (nom, quant) {
-        let produtoExistente = this.produtos.find(p => p.nome === nom)
-
-        if (quant <= 0) {
-            console.log("Falha ao Remover Produto - Digite uma Quantidade Válida!")
-            console.log("--------------------")
-            return
-        }
-
-        if (!produtoExistente) {
-            console.log("Falha ao Remover Produto - Item não Existe em estoque!")
-            console.log("--------------------")
-            return
-        }
-
-        if (quant > produtoExistente.quantidade) {
-            console.log("Falha ao Remover Produto - Quantidade em Estoque Insuficiente!")
-            console.log(`Estoque Real... Produto:${produtoExistente.nome} - Quantidade: ${produtoExistente.quantidade}`)
-            console.log("--------------------")
-            return
-        }
-
-        produtoExistente.quantidade -= quant
-        console.log(`Quantidade de ${produtoExistente.nome} Atualizada -${quant}`)
-        console.log("--------------------")
-
-        if(produtoExistente.quantidade===0){
-            this.produtos=this.produtos.filter((p=>p.nome !==nom))
-            console.log(`Produto ${nom} Zerado e Removido do Estoque! `)
-            console.log("--------------------")
-        }
-
     }
-
-
 }
 
-estoque.adicionarProduto("playStation", 30)
-estoque.adicionarProduto("playStation", 30)
-estoque.adicionarProduto("playStation", 0)
-estoque.listarProdutos()
-estoque.removerProduto("playStation", 30)
-estoque.removerProduto("playStation", 30)
+estoque.adicionarProduto("mouse", 10)
+estoque.adicionarProduto("teclado", 20)
+estoque.adicionarProduto("teclado", -20)
 estoque.listarProdutos()
