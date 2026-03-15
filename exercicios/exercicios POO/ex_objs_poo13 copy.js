d38fc2acfc36ae6643b8ba9330c0c5f0198a6fca
marcelo c. amorim <marceloamorim.dev@gmail.com>
2026-03-15 14:35:52 -0300

    praticando js
    ex 13 refeito

--- exercicios/exercicios POO/ex_objs_poo13 copy.js
+++ exercicios/exercicios POO/ex_objs_poo13 copy.js
@@ -8,54 +8,100 @@
 // mostrarPedidos() → lista todos os pratos e quantidades.
 // 🧠 Desafio extra: evite pedidos duplicados (se já existir, apenas aumente a quantidade).
 
-const restaurante={
-    cardapio:[
-        {nome:"pizza", preco:80},
-        {nome:"pastel", preco:15},
-        {nome:'esfiha', preco:5},
-        {nome:'pudim', preco:10},
+const restaurante = {
+    cardapio: [
+        { nome: "coxinha", preco: 10 },
+        { nome: "suco", preco: 8 },
+        { nome: "pastel", preco: 6 },
+    ],
+    pedidos: [
+        //nome,quantidade,preco,subtotal
     ],
 
-    //nome,quantidade
-    pedidos:[],
 
-    fazerPedido:function(nome,quanti){
-        const itemExistente=this.cardapio.find(item=>item.nome===nome)
-        if(!itemExistente){
-            console.log(`ERRO - Item ${nome} Não consta no Cardapio`)
+    separar: function () {
+        console.log(`---------------------------------`)
+    },
+
+    fazerPedido: function (nomePrato, quantidade) {
+        const itemExisteCardapio = this.cardapio.find(item => item.nome === nomePrato)
+        if (!itemExisteCardapio) {
+            console.log(`Item "${nomePrato}" não consta no cardapio`)
+            this.separar()
             return
         }
 
-        let valorTotal = itemExistente.preco * quanti
+        const itemExistePedido = this.pedidos.find(item => item.nome === nomePrato)
+        let subtotalItem = itemExisteCardapio.preco * quantidade
 
-        const ItemPedidoExistente = this.pedidos.find(item=>item.nome===nome)
-        if(!ItemPedidoExistente){
-            this.pedidos.push({nome:nome, quantidade:quanti, valor:valorTotal})
-            console.log(`Pedido Registrado! Item: ${nome} - x${quanti}`)
-        }else{
-            ItemPedidoExistente.quantidade += quanti
-            ItemPedidoExistente.valor += valorTotal
-            console.log(`Pedido Atualizado! Item: ${nome} +${quanti}`)
+        if (!itemExistePedido) {
+            this.pedidos.push({ nome: nomePrato, quantidade: quantidade, preco: itemExisteCardapio.preco, subtotal: subtotalItem })
+            console.log(`Pedido realizado com sucesso!`)
+            this.separar()
+            return
         }
+        itemExistePedido.quantidade += quantidade
+        itemExistePedido.subtotal += subtotalItem
+        console.log(`Pedido Atualizado "+${quantidade}x ${nomePrato}"`)
+        this.separar()
+
     },
 
-    calcularTotal:function(){
-        let total = this.pedidos.reduce((acc,item)=>acc+item.valor,0)
-        return total
+    editarPedido: function (nomePrato, quantidade) {
+
+        const itemExistePedido = this.pedidos.find(item => item.nome === nomePrato)
+        if (!itemExistePedido) {
+            console.log(`Item "${nomePrato}" não consta no Pedido`)
+            this.separar()
+            return
+
+        } else if (quantidade <= 0) {
+            console.log(`ERRO - Digite uma quantidade válida!`)
+            this.separar()
+            return
+
+        } else if (quantidade >= itemExistePedido.quantidade) {
+            if(quantidade>itemExistePedido.quantidade){
+                console.log(`Quantidade solicitada é maior que a quantidade do pedido`)
+                console.log(`O item "${nomePrato}" será exluído`)
+                this.separar()
+            }
+            console.log(`Aquantidade do item foi zerada`)
+            console.log(`Item ${nomePrato} Excluido com sucesso!`)
+            this.pedidos=this.pedidos.filter((item=>item.nome!==nomePrato))
+            this.separar()
+            return
+
+        }
+
+        let subtotalItem = itemExistePedido.preco * quantidade
+
+        console.log(`Pedido Editado com sucesso - "-${quantidade}x ${nomePrato}"`)
+        itemExistePedido.quantidade -= quantidade
+        itemExistePedido.subtotal -= subtotalItem
+        this.separar()
     },
 
-    resumoPedido:function(){
-        console.log(`Resumo do Pedido`)
-        let totalPagar=this.calcularTotal()
+    resumoPedido(){
+        let totalPagar = this.pedidos.reduce((acc,item)=>acc + item.subtotal,0)
+        console.log(`Resumo do pedido:`)
         this.pedidos.forEach((el)=>{
-            console.log(`Item: ${el.nome} - Quantidade: ${el.quantidade} - Valor: R$${el.valor.toFixed(2)}`)
+            console.log(`--Item:${el.nome} - Quantidade: ${el.quantidade} - Preço: R$${el.preco.toFixed(2)} - Total do Item: R$${el.subtotal.toFixed(2)}`)
         })
-        console.log(`Total a Pagar: R$${totalPagar.toFixed(2)}`)
+        console.log(`Total a pagar R$${totalPagar.toFixed(2)}`)
     }
 }
 
-restaurante.fazerPedido("pizza",2)
-restaurante.fazerPedido("pizza",2)
-restaurante.fazerPedido("pudim",5)
-restaurante.fazerPedido("salada",3)
+restaurante.fazerPedido("sopa")
+restaurante.fazerPedido("coxinha", 2)
+restaurante.fazerPedido("coxinha", 2)
+restaurante.fazerPedido("coxinha", 4)
+restaurante.editarPedido("coxinha", 2)
+restaurante.editarPedido("coxinha", 2)
+restaurante.editarPedido("coxinha", 2)
+restaurante.editarPedido("coxinha", 2)
+
+restaurante.fazerPedido("pastel",5)
+restaurante.fazerPedido("suco",5)
+restaurante.fazerPedido("coxinha",10)
 restaurante.resumoPedido()
